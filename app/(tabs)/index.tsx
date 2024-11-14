4abed9320c47662e07ea2d2ddbe8d789c67293fd
Collins <collins.olix@gmail.com>
2024-11-14 19:27:30 +0200

    I have no Idea what this update is

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,165 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, View, Text, SectionList } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  FlatList,
+  GestureHandlerRootView,
+  Pressable,
+} from "react-native-gesture-handler";
+import { Quicksand_600SemiBold, useFonts } from "@expo-google-fonts/quicksand";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function HomeScreen() {
+  let [fontsLoaded, error] = useFonts({
+    Quicksand_600SemiBold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+  const DATA = [
+    {
+      title: "Main dishes",
+      data: [
+        {
+          title: "Pizza",
+          id: "pizza",
+          pic: require("@/assets/images/placeholder.png"),
+        },
+        {
+          title: "Burger",
+          id: "burger",
+          pic: require("@/assets/images/placeholder.png"),
+        },
+        {
+          title: "Risotto",
+          id: "risotto",
+          pic: require("@/assets/images/placeholder.png"),
+        },
+        {
+          title: "Wraps",
+          id: "wraps",
+          pic: require("@/assets/images/placeholder.png"),
+        },
+      ],
+    },
+    // {
+    //   title: "Sides",
+    //   data: [
+    //     { title: "French Fries", id: "French Fries" },
+    //     { title: "Onion Rings", id: "Onion Rings" },
+    //     { title: "Fried Shrimps", id: "Fried Shrimps" },
+    //   ],
+    // },
+    // {
+    //   title: "Drinks",
+    //   data: [
+    //     { title: "Water", id: "Water" },
+    //     { title: "Coke", id: "Coke" },
+    //     { title: "Beer", id: "Beer" },
+    //   ],
+    // },
+    // {
+    //   title: "Desserts",
+    //   data: ["Cheese Cake", "Ice Cream"],
+    // },
+  ];
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.safeAreaView}>
+      <GestureHandlerRootView>
+        <View style={styles.profileNav}>
+          <View>
+            <Text
+              style={{
+                fontFamily: "Quicksand_600SemiBold",
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              SomeText
+            </Text>
+          </View>
+          <View>
+            <Text
+              style={{
+                fontFamily: "Poppins",
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              SomeText
+            </Text>
+          </View>
+          <View></View>
+        </View>
+        <View style={styles.bottomModal}>
+          <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => item + index}
+            // renderItem={({ item }) => (
+            //   <View style={styles.item}>
+            //     <Text style={styles.title}>{item}</Text>
+            //   </View>
+            // )}
+            renderItem={() => <></>}
+            renderSectionHeader={({ section }) => (
+              <View style={{ margin: 10 }}>
+                <Text style={styles.itemHearders}>{section.title}</Text>
+                <FlatList
+                  horizontal
+                  data={section.data}
+                  renderItem={({ item }) => (
+                    <View>
+                      <Text>{item.title}</Text>
+                      <Pressable style={styles.homeScreenPressables}>
+                        <Image source={item.pic} style={styles.images} />
+                      </Pressable>
+                    </View>
+                  )}
+                />
+              </View>
+            )}
+          />
+        </View>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: "#ff036a",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  profileNav: {
+    height: "20%",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  bottomModal: {
+    flex: 1,
+    paddingTop: 10,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    elevation: 7,
+  },
+  itemHearders: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  homeScreenPressables: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+    backgroundColor: "#4e4e4e30",
+    marginRight: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  images: {
+    flex: 1,
+    resizeMode: "contain",
   },
 });
